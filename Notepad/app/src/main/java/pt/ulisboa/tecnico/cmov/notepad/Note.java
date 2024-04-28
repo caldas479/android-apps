eac094438b53c2de4528a16c0cdbd8ca0691ceed
ist199125 <tiago.rodrigues.caldas@tecnico.ulisboa.pt>
2024-04-28 01:58:57 +0100

    Notepad base code

---
+++ Notepad/app/src/main/java/pt/ulisboa/tecnico/cmov/notepad/Note.java
@@ -0,0 +1,29 @@
+package pt.ulisboa.tecnico.cmov.notepad;
+
+public class Note {
+    private String title;
+    private String text;
+
+    public Note(String title, String text) {
+        this.title = title;
+        this.text = text;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+}
+
